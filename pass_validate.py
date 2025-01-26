4cfd5c789b64ebc14f2be781034e2b64526591b9
raji2306 <94977452+raji2306@users.noreply.github.com>
2025-01-26 21:51:41 +0530

    Create pass_validate.py

---
+++ pass_validate.py
@@ -0,0 +1,29 @@
+import sys
+import requests
+import json
+from requests.auth import HTTPBasicAuth
+from urllib.parse import quote
+import re
+import json
+import os
+
+# Retrieve environment variables
+user = os.getenv('BINDUSER')
+password = os.getenv('BINDPASSWORD')
+tool_user = os.getenv('TOOLUSER')
+tool_password = os.getenv('TOOLPASS')
+
+a = user
+b = password 
+c = tool_user
+d= tool_password 
+
+print(f"User: {a}")
+print(f"Password: {b}")
+print(f"Tool_User: {c}")
+print(f"Tool_Password: {d}")
+
+if a == "user()*" or b =="pass" :
+    print("Authentication successful!") 
+else :
+    print("Failed")
